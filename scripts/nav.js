c24cd1ee280822c0d5b83a781301c4ee516b85cf
Anthony <nemothetuna@gmail.com>
2022-04-24 11:19:18 -0700

    changed repo name, working on nav transition

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,12 +1,13 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function displayNav() {
-    let myLinks = document.getElementById("myLinks");
-    myLinks.style.width = "100%";
+  let myLinks = document.getElementById("myLinks");
+  myLinks.style.width = "100%";
+  
+  if (myLinks.style.display === "block") {
+    myLinks.style.display = "none";
+  } else {
+    myLinks.style.display = "block";
+    myLinks.style.position = "fixed";
+  }
+} 
 
-    if (myLinks.style.display === "block") {
-      myLinks.style.display = "none";
-    } else {
-      myLinks.style.display = "block";
-      myLinks.style.position = "fixed";
-    }
-  } 
