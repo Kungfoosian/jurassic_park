47d4c76a43b99475b92b9ee7f4bea7253b10256f
Anthony <nemothetuna@gmail.com>
2022-05-01 13:07:59 -0700

    finished animating nav burger button

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,22 +1,18 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function displayNav() {
-  let myLinks = document.getElementById("myLinks");
-  myLinks.style.width = "100%";
-  
-  if (myLinks.style.display === "block") {
-    myLinks.style.display = "none";
-  } else {
-    myLinks.style.display = "block";
-    // myLinks.style.position = "absolute";
-  }
-}
-
 // Animation for hamburger
+const navTop = document.getElementById('navTop');
+const logo = document.getElementById('logo');
 const hamburgerNav = document.getElementById('nav-toggle');
 const hamburgerTop = document.getElementById('burger-top');
 const hamburgerBottom = document.getElementById('burger-bottom');
+const dropdown = document.getElementById('dropdown');
 
 hamburgerNav.addEventListener('click', e => {
+  navTop.classList.toggle('bg-white');
+  dropdown.classList.toggle('bg-white');
+  logo.classList.toggle('filter-dark');
+
+  dropdown.classList.toggle('hidden');
+  dropdown.classList.toggle('display');
 
   hamburgerTop.classList.toggle('top-normal');  
   hamburgerTop.classList.toggle('top-cross');  
